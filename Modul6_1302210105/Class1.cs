230d4c88bdeaba9467def375f6328879924192d0
Onesiforus-Rade-Putra <onesiforusradeputra@gmail.com>
2023-03-29 17:06:09 +0700

    Console.WriteLine("Review film : " + item.getTitle());

---
+++ Modul6_1302210105/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul6_1302210105
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random rnd = new Random();
+            this.title = title;
+            this.playCount = 0;
+            this.id = rnd.Next(10000);
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount = playCount;
+        }
+        
+        public void PrintAllVideoPlaycount()
+        {
+            Console.WriteLine("video memiliki judul: " + title);
+            Console.WriteLine("video memiliki id: " + id);
+            Console.WriteLine("video diputar sebanyak: " + playCount);
+        }
+
+        public int getplayCount()
+        {
+            return this.playCount;
+        }
+        public string getTitle()
+        {
+            return this.title;
+        }
+
+    }
+
+    internal class SayaTubeUser
+    {
+        private int id;
+        private string username;
+        private List<SayaTubeVideo> uploadedVideo;
+        private string v;
+
+        public SayaTubeUser(string v)
+        {
+            this.v = v;
+            this.uploadedVideo= new List<SayaTubeVideo>();
+        }
+
+        public int GetTotalVideoCount()
+        {
+            int totalVideoCount = 0;
+            foreach (SayaTubeVideo item in uploadedVideo)
+            {
+                totalVideoCount = item.getplayCount() + totalVideoCount;
+            }
+            return totalVideoCount;
+
+        }
+
+        public void addVideo(SayaTubeVideo video)
+        {
+           uploadedVideo.Add(video);
+        }
+        public void PrintAllVideoPlaycount()
+        {
+            Console.WriteLine("user :" + username);
+            int temp = 0;
+            foreach(SayaTubeVideo item in uploadedVideo)
+            {
+                Console.WriteLine("Review film : " + item.getTitle());
+                Console.WriteLine("video ditonton sebanyak : " + item.getplayCount());
+                temp++;
+            }
+        }
+    }
+}
--- Modul6_1302210105/Program.cs
+++ Modul6_1302210105/Program.cs
@@ -1,4 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Modul6_1302210105;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeUser user = new SayaTubeUser("Mahiro");
+        SayaTubeVideo video = new SayaTubeVideo("Angel Next Door Season 1");
+        video.IncreasePlayCount(1);
+        user.addVideo(video);
+        SayaTubeVideo video1 = new SayaTubeVideo("Angel Next Door Season 2");
+        video1.IncreasePlayCount(2);
+        user.addVideo(video1);
+        SayaTubeVideo video2 = new SayaTubeVideo("Angel Next Door Season 3");
+        video2.IncreasePlayCount(3);
+        user.addVideo(video2);
+        SayaTubeVideo video3 = new SayaTubeVideo("Angel Next Door Season 4");
+        video3.IncreasePlayCount(4);
+        user.addVideo(video3);
+        SayaTubeVideo video4 = new SayaTubeVideo("Angel Next Door Season 5");
+        video4.IncreasePlayCount(5);
+        user.addVideo(video4);
+        SayaTubeVideo video5 = new SayaTubeVideo("Angel Next Door Season 6");
+        video5.IncreasePlayCount(6);
+        user.addVideo(video5);
+        SayaTubeVideo video6 = new SayaTubeVideo("Angel Next Door Season 7");
+        video6.IncreasePlayCount(7);
+        user.addVideo(video6);
+        SayaTubeVideo video7 = new SayaTubeVideo("Angel Next Door Season 8");
+        video7.IncreasePlayCount(8);
+        user.addVideo(video7);
+        SayaTubeVideo video8 = new SayaTubeVideo("Angel Next Door Season 9");
+        video8.IncreasePlayCount(9);
+        user.addVideo(video8);
+        SayaTubeVideo video9 = new SayaTubeVideo("Angel Next Door Season 10");
+        video9.IncreasePlayCount(10);
+        user.addVideo(video9);
+
+
+
+
+        user.PrintAllVideoPlaycount();
+
+    }
+}
 
 
